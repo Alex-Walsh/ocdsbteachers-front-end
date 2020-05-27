499d93556b444ac46f08ac1f6056adf52982163f
Alex Walsh <alexwalsh350@gmail.com>
2020-05-26 22:09:41 -0400

    first commit

---
+++ precache-manifest.a287372dbdd1d38462a95d1a9c35b2bd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6e0b32d755c1f2e4e29f024c16b8f46",
+    "url": "/index.html"
+  },
+  {
+    "revision": "99d6aaa02571ad079a84",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "fb5aff3f72dd314dae99",
+    "url": "/static/css/main.02518f4d.chunk.css"
+  },
+  {
+    "revision": "99d6aaa02571ad079a84",
+    "url": "/static/js/2.09b3686b.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.09b3686b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb5aff3f72dd314dae99",
+    "url": "/static/js/main.a15c032f.chunk.js"
+  },
+  {
+    "revision": "256678927136c25d4fd5",
+    "url": "/static/js/runtime-main.fe5197cd.js"
+  }
+]);
